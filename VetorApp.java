69ca361a263bf33134986d1aaf8193a0b1ad02ca
ian <80067694+iansergio@users.noreply.github.com>
2025-03-29 11:58:05 -0300

    Adicionando estrutura inicial

---
+++ VetorApp.java
@@ -0,0 +1,19 @@
+import estruturas.Vetor;
+
+public class VetorApp {
+
+    public static void main(String[] args) {
+        Vetor vet = new Vetor(10);
+
+        vet.adicionar(2);
+        vet.adicionar(7);
+        vet.adicionar(9);
+        vet.adicionar(12);
+        vet.adicionar(16);
+        vet.adicionar(21);
+        vet.adicionar(27);
+        vet.adicionar(33);
+        vet.adicionar(42);
+        vet.adicionar(54);
+    }
+}
